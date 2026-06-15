787e5634d248e1f3081cca1ca77b17de7b17b3e1
EA - João Pedro Gomes Augusto Gesteira <joao_gesteira@educadventista.org>
2026-06-15 15:41:38 -0300

    Projeto finalizado

--- main.js
+++ main.js
@@ -27,24 +27,36 @@ function mostrarListaAtualizada(){
     const sectionLista = document.querySelector(".lista")
 
     sectionLista.innerHTML = ""
+
+    itens.sort((itemA, itemB) => Number(itemA.marcado) - Number(itemB.marcado))
     
     for(let i = 0; i < itens.length; i++){
         sectionLista.innerHTML += `
             <div class="item">
                 <div>
-                    <input type="checkbox" id="item-${i}">
+                    <input type="checkbox" id="item-${i}" ${itens[i].marcado === true && "checked"}>
 
-                    <div class="checkbox-customizada">
-                        <img src="./img/checked.svg" alt="checked icon">
-                    </div>
-
-                    <label for="item-${i}">${itens[i].descricao}</label>
+                <div class="checkbox-customizada" onclick="marcarItem(${i})">
+                    <img src="./img/checked.svg" alt="checked">
                 </div>
 
-                <button>
-                    <img src="./img/trash-icon.svg" alt="imagem lixo">
+                <label for="item-${i}" onclick="marcarItem(${i})">${itens[i].descricao}</label>
+                </div>
+
+                <button onclick="apagarItem(${i})">
+                    <img src="./img/trash-icon.svg" alt="ícone do lixo">
                 </button>
             </div>
         `
     }
+}
+
+function apagarItem(indice) {
+    itens.splice(indice, 1)
+    mostrarListaAtualizada()
+}
+
+function marcarItem(indice) {
+    itens[indice].marcado = !itens[indice].marcado
+    mostrarListaAtualizada()
 }
